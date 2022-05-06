3227cb5bfb5cf7f239d3115415c0451078bae493
Zekun Jiang <37135971+JZK00@users.noreply.github.com>
2022-05-06 13:11:50 +0800

    Add files via upload

---
+++ Stu1/LearnPython/function/compare.py
@@ -0,0 +1,10 @@
+def max(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+
+a = 4
+b = 5
+print(max(a, b))
---
+++ Stu1/LearnPython/function/return_statement.py
@@ -0,0 +1,11 @@
+# 可写函数说明
+def sum(arg1, arg2):
+    # 返回2个参数的和."
+    total = arg1 + arg2
+    print("函数内 : ", total)
+    return total
+
+
+# 调用sum函数
+total = sum(10, 20)
+print("函数外 : ", total)
